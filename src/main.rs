d586ea6cb0bff2d13078b473c90882731b1ed718
Sam Bossley <35381535+bossley9@users.noreply.github.com>
2020-12-15 23:30:56 -0500

    Basic input handling in rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Search:");
+
+    let mut input = String::new();
+
+    match io::stdin().read_line(&mut input) {
+        Ok(n) => {
+            println!("bytes: {}", n);
+            println!("input: {}", input);
+        }
+        Err(error) => println!("error: {}", error),
+    }
+
+    println!("You inputted: {}", input.trim());
 }
