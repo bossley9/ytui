c3f906886f3c7d577eabb58fb54ad7cf2c8c7833
Sam Bossley <35381535+bossley9@users.noreply.github.com>
2020-12-18 10:13:33 -0500

    Encode query and construct url

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,20 @@ use std::env;
 const RED: &str = "\x1b[31m";
 const NC: &str = "\x1b[0m";
 
-// const FQDN: &str = "https://youtube.com";
-// const PATH_SEARCH_RESULTS: &str = "results?search_query=";
+const FQDN: &str = "https://youtube.com";
+const PATH_SEARCH_RESULTS: &str = "results?search_query=";
+
+fn encode(unencoded_url: &mut String) {
+    *unencoded_url = str::replace(unencoded_url, "%", "%25");
+    *unencoded_url = str::replace(unencoded_url, " ", "%20");
+    *unencoded_url = str::replace(unencoded_url, "'", "%91");
+}
 
 fn main() {
     // search command line args
     let args = env::args().skip(1);
 
-    let search_str: String = args
+    let mut search_str: String = args
         .fold(String::new(), |acc, el| acc + &el + " ")
         .trim()
         .to_string();
@@ -20,13 +26,16 @@ fn main() {
         return;
     }
 
-    println!("search_str is \"{}\"", search_str);
-
     // encode search query
+    encode(&mut search_str);
 
     // construct url
+    let mut url = String::from(FQDN);
+    url.push_str(PATH_SEARCH_RESULTS);
+    url.push_str(&search_str);
 
     // fetch data (with error handling)
+    println!("url is \"{}\"", url);
 
     // 1. condense to a single line (remove new line chars)
     // 2. trim start up to "var ytInitialData = "
